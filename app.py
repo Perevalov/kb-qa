97d961a5bf328cb6f0657fbf29b0ef6b908db9e3
Перевалов Александр Максимович <perevalova@pstu.ru>
2019-11-07 00:15:20 +0100

    added new types of questions. not completed yet

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
 from flask import Flask, render_template, request, flash, session, url_for, redirect, make_response
-from passlib.hash import sha256_crypt
 from datetime import timedelta, datetime
-import requests
+from managers.DialogueManager import DialogueManager
+from nlu.classifiers.KeywordClassifier import  KeywordClassifier
+from resources import constants
 from SPARQLWrapper import SPARQLWrapper, JSON
-import spacy
 
-nlp = spacy.load("en_core_web_sm")
 app = Flask(__name__)
 app.secret_key = str(datetime.now().time())
 
-def get_cities_for_a_text(text):
-    doc = nlp(text)
-
-    cities = list()
-    for ent in doc.ents:
-        if ent.label_ == 'GPE':
-            cities.append(ent.text)
-
-    return cities
+classifier = KeywordClassifier(constants.CLASSES)
+dialogue_manager = DialogueManager(classifier=classifier)
 
 @app.route("/")
 def index():
@@ -27,43 +19,16 @@ def index():
 def get_answer():
     print("get_answer Started")
     question_text = request.form.get("question_text")
-    supported_question_prefix = "How many people live in "
-    cities = get_cities_for_a_text(question_text)
+
+    answer_text = dialogue_manager.get_answer(question_text)
+
     response_array = list()
 
-    if question_text.lower().strip().startswith(supported_question_prefix.lower()) and len(cities) > 0:
+    """for result in results["results"]["bindings"]:
+        response_array.append({'City Label': result['cityLabel']['value'],
+                            'Num. of Inhabitants': result['numberOfInhabitants']['value']})"""
 
-        sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-        sparql.setQuery("""
-            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-            PREFIX dbo: <http://dbpedia.org/ontology/>
-            PREFIX yago: <http://dbpedia.org/class/yago/>
-            SELECT DISTINCT ?cityLabel ?numberOfInhabitants
-            WHERE {
-                    VALUES ?placeType { dbo:City yago:City108524735 }
-                    ?s rdf:type ?placeType .
-                    ?s rdfs:label ?cityLabel .
-                    filter(contains(lcase(?cityLabel),"%s")) .
-                    filter(LANG(?cityLabel) = "en") .
-                    ?s dbo:populationTotal ?numberOfInhabitants .  
-            }
-            ORDER BY DESC (?numberOfInhabitants)
-            LIMIT 100
-        """ % (cities[0].lower()))
-
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
-
-        for result in results["results"]["bindings"]:
-            response_array.append({'City Label': result['cityLabel']['value'],
-                                'Num. of Inhabitants': result['numberOfInhabitants']['value']})
-    else:
-        response_array.append({'Question \"%s\" is not supported.' % (question_text): 'Only questions starting with the phrase \"%s\" and containing a city name might be evaluated.' % (supported_question_prefix)})
-
-    session['response'] = response_array
-
-    print("get_answer Ended")
+    session['response'] = answer_text
 
     return redirect(url_for('index'))
 
